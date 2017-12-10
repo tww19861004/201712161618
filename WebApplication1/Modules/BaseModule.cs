6b1cd38e1f841ebc6a345383969a830d6c336a22
唐伟伟 <tww24098@ly.com>
2017-12-10 19:15:35 +0800

    tww 20181210

---
+++ WebApplication1/Modules/BaseModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using System.Dynamic;
+
+namespace NancyWebTest.Module
+{
+    public class BaseModule : NancyModule
+    {
+        //声明动态对象，用于控制器绑定数据传递到页面
+        public dynamic DynamicModel = new ExpandoObject();
+
+        public BaseModule()
+        {
+            SetupModelDefaults();
+        }
+
+        private void SetupModelDefaults()
+        {
+            Before += ctx =>
+            {
+                return null;
+            };
+        }
+    }
+}
---
+++ WebApplication1/Modules/HomeModule.cs
@@ -0,0 +1,75 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyWebTest.Module
+{
+    public class TestClass
+    {
+        public TestClass()
+        { }
+        public TestClass(string id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class HomeModule : BaseModule
+    {
+        public HomeModule()
+        {
+            //首页
+            Get["/nancy/Home"] = parameters => ReturnHomeAction();
+            //返回字符串演示
+            Get["/nancy/getStringValue"] = parameters => ReturnStringAction();
+            //重定向页面演示
+            Get["/nancy/redirectOtherPage"] = parameters => ReturnRedirectAction();
+        }
+        public dynamic ReturnHomeAction()
+        {
+            //单一数值
+            DynamicModel.HelloWorld = "Hello world...";
+            //集合数据 1
+            List<string> list1 = new List<string>() { "listValue_1", "listValue_2", "listValue_3", "listValue_4" };
+            //集合数据 2 
+            List<TestClass> list2 = new List<TestClass>() {
+                new TestClass("1","张三"),
+                new TestClass("2","李四"),
+                new TestClass("3","王五")
+            };
+            DynamicModel.List1 = list1;
+            DynamicModel.List2 = list2;
+            return View["Home", DynamicModel];
+        }
+        public dynamic ReturnStringAction()
+        {
+            List<TestClass> list2 = new List<TestClass>() {
+                new TestClass("1","张三"),
+                new TestClass("2","李四"),
+                new TestClass("3","王五") };
+
+            return Response.AsJson<List<TestClass>>(list2);
+            //return "这里一般是一个json串，常用于ajax异步处理，返回json串后页面解析操作等";
+        }
+        public dynamic ReturnRedirectAction()
+        {
+            //重定向跳转 1==1 模拟判断用户登录状态有效
+            if (1 == 1)
+            {
+                //当前用户登录状态有效
+                return Response.AsRedirect("/Login");
+            }
+            else
+            {
+                //跳转到登录页面
+                return Response.AsRedirect("/Login");
+            }
+            return null;
+        }
+    }
+}
