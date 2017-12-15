278611f1140463b7758e232bf096266fb1c65d59
唐伟伟 <tww24098@ly.com>
2017-12-15 18:28:43 +0800

    lucene.net

--- NPocoTest/Program.cs
+++ NPocoTest/Program.cs
@@ -20,7 +20,32 @@ namespace NPocoTest
             using (IDatabase db = new Database(connectionString, NPoco.DatabaseType.SQLite))
             {
                 List<User> users = db.Fetch<User>("select * from user");
+
+                User u = new User();
+                //u.Id = i;
+                u.Name = "1";
+                u.Phone = "15062437243";
+                u.Email = "382233701@qq.com";
+                u.Password = "1234";
+                u.CreateTime = DateTime.Now.ToString("yyyy-MM-dd");
             }
+
+            QueryAsync();
+        }
+
+        public static async Task<List<User>> QueryAsync()
+        {
+            return await Task.Run<List<User>>(()=>
+            {
+                String fileName = Path.Combine(Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "tww1.db");
+                // create a database "context" object t
+                String connectionString = @"Data Source=" + fileName + ";Version=3;";
+
+                using (IDatabase db = new Database(connectionString, NPoco.DatabaseType.SQLite))
+                {
+                    return Task<List<User>>.Run(() => { return db.Query<User>().Where(x => x.Id == 1).ToListAsync(); });
+                }                
+            });           
         }
     }
 }
