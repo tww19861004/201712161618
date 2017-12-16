55bd678cda86441a1dd465b1b522e7faab66077e
唐伟伟 <tww24098@ly.com>
2017-12-16 22:39:38 +0800

    async await

---
+++ AsyncAwaitTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTest
+{
+    class Program
+    {
+        //https://www.cnblogs.com/jesse2013/p/async-and-await.html
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Main Thread Id: {0}\r\n", Thread.CurrentThread.ManagedThreadId);
+            Test();
+            Console.ReadLine();
+        }
+
+        static async Task Test()
+        {
+            await Task.Delay(10000);
+
+            Console.WriteLine("Before calling GetName, Thread Id: {0}\r\n", Thread.CurrentThread.ManagedThreadId);
+            var name = GetName();   //我们这里没有用 await,所以下面的代码可以继续执行
+                                    // 但是如果上面是 await GetName()，下面的代码就不会立即执行，输出结果就不一样了。
+            Console.WriteLine("End calling GetName.\r\n");
+            Console.WriteLine("Get result from GetName: {0}", await name);
+        }
+
+        static async Task<string> GetName()
+        {
+            // 这里还是主线程
+            Console.WriteLine("Before calling Task.Run, current thread Id is: {0}", Thread.CurrentThread.ManagedThreadId);
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("'GetName' Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
+                return "tww";
+            });
+        }
+    }
+}
