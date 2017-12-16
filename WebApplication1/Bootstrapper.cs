45d41f9fe4d0f1cc1d01bcf9f8676f59b0e41d78
唐伟伟 <tww24098@ly.com>
2017-12-16 11:14:24 +0800

    Nancy_GZIP

---
+++ WebApplication1/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Nancy.Json;
+using Nancy;
+using Nancy.Session;
+using Nancy.Security;
+using System.IO;
+
+namespace WebApplication1
+{
+    
+    public class Bootstrapper : Nancy.DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            // Enable Compression with Settings
+            //var settings = new GzipCompressionSettings();
+            //settings.MinimumBytes = 1024;
+            //settings.MimeTypes.Add("application/vnd.myexample");
+            //pipelines.EnableGzipCompression(settings);
+
+            // Enable Compression with Default Settings
+            pipelines.EnableGzipCompression();
+
+            base.ApplicationStartup(container, pipelines);
+            //JsonSettings.DefaultCharset = "utf-8";
+            //Nancy.Json.JsonSettings.MaxJsonLength = 20971520;
+            //StaticConfiguration.EnableRequestTracing = true;
+            //StaticConfiguration.DisableErrorTraces = false;
+            //Csrf.Enable(pipelines);
+            //CookieBasedSessions.Enable(pipelines);
+        }
+    }
+}
---
+++ WebApplication1/GzipCompressionSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1
+{
+    using System.Collections.Generic;
+
+    public class GzipCompressionSettings
+    {
+        public int MinimumBytes { get; set; } = 4096;
+
+        public IList<string> MimeTypes { get; set; } = new List<string>
+        {
+            "text/plain",
+            //"text/html",
+            //"text/xml",
+            //"text/css",
+            //"application/json",
+            //"application/x-javascript",
+            //"application/atom+xml",
+            //"application/xml"
+        };
+    }
+}
