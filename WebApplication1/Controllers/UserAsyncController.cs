c1c9196d7cd7e86daf21015d87683dab6e7b0855
唐伟伟 <tww24098@ly.com>
2017-12-17 14:31:03 +0800

    get the user by id

--- WebApplication1/Controllers/UserAsyncController.cs
+++ WebApplication1/Controllers/UserAsyncController.cs
@@ -25,18 +25,32 @@ namespace WebApplication1.Controllers
                 var res = await UserService.GetAllUsersAsync(ct);
                 return Response.AsJson(res);
             };
-            //test
-            Get["/1"] = _ =>
+            #region NegotiatorExtensions.test
+            Get["/test"] = _ =>
             {
                 var res = UserService.GetAllUsers().First();
                 return NegotiatorExtensions.WithModel(Negotiate.WithStatusCode(HttpStatusCode.OK), res);
             };
+            #endregion
             //get the user by id
             Get["/{id:int}", runAsync: true] = async (x, ct) =>
             {
                 var res1 = await UserService.GetUserByIdAsync(ct,x.id);
-                return NegotiatorExtensions.WithModel(Negotiate.WithStatusCode(HttpStatusCode.OK), res1);                
+                Nancy.Json.JavaScriptSerializer js = new Nancy.Json.JavaScriptSerializer();
+                return js.Serialize(res1);
             };
+
+            //return "Received POST request";
+            //var id = this.Request.Body;
+            //var length = this.Request.Body.Length;
+            //var data = new byte[length];
+            //id.Read(data, 0, (int)length);
+            //var body = System.Text.Encoding.Default.GetString(data);
+
+            //var request = JsonConvert.DeserializeObject<SimpleRequest>(body);
+
+            //return 200;
+
         }
     }
 }
