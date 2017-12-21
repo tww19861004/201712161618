cefd76a6f92030b1a9668c9850e36b648c07c08b
唐伟伟 <tww24098@ly.com>
2017-12-21 11:00:12 +0800

    protobuf with gzip

--- Protobuf-new/Program.cs
+++ Protobuf-new/Program.cs
@@ -28,6 +28,23 @@ namespace Protobuf_new
 
             System.Threading.Thread.Sleep(100);
 
+            using (HttpClient hp1 = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression =
+    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+            }))
+            {
+                hp1.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                string test1 = "http://localhost:12018//userasync/newtonjsontest";
+                sw.Reset();
+                sw.Start();
+                var res1 = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(hp1.GetStringAsync(test1).Result);
+                sw.Stop();
+                Console.WriteLine("newtonsoft with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
+            }
+
+            System.Threading.Thread.Sleep(100);
+
             using (HttpClient hp = new HttpClient())
             {
                 hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
@@ -37,7 +54,24 @@ namespace Protobuf_new
                 var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
                 sw.Stop();
                 Console.WriteLine("protobuf一共耗时:" + sw.Elapsed.TotalMilliseconds+"，返回："+res.Count.ToString());
-            }                            
+            }
+
+            System.Threading.Thread.Sleep(100);
+
+            using (HttpClient hp = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression =
+    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+            }))
+            {
+                hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
+                string test = "http://localhost:12018/protobufnet/2019";
+                sw.Reset();
+                sw.Start();
+                var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
+                sw.Stop();
+                Console.WriteLine("protobuf with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res.Count.ToString());
+            }
             Console.ReadKey();
         }
     }
