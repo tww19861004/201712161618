5b077ad104519e76d34eed77c92092cdc36fe713
唐伟伟 <tww24098@ly.com>
2017-12-27 20:00:44 +0800

    update  vs2015 to vs2017

--- Protobuf-new/Program.cs
+++ Protobuf-new/Program.cs
@@ -13,65 +13,7 @@ namespace Protobuf_new
     {
         static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch();
-
-            using (HttpClient hp = new HttpClient())
-            {
-                hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
-                string test = "http://localhost:12018/protobufnet/2019";
-                sw.Reset();
-                sw.Start();
-                var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
-                sw.Stop();
-                Console.WriteLine("protobuf一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res.Count.ToString());
-            }
-
-            System.Threading.Thread.Sleep(100);
-
-            using (HttpClient hp = new HttpClient(new HttpClientHandler
-            {
-                AutomaticDecompression =
-    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
-            }))
-            {
-                hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
-                string test = "http://localhost:12018/protobufnet/2019";
-                sw.Reset();
-                sw.Start();
-                var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
-                sw.Stop();
-                Console.WriteLine("protobuf with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res.Count.ToString());
-            }
-            System.Threading.Thread.Sleep(100);
-            using (HttpClient hp1 = new HttpClient())
-            {
-                hp1.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
-                string test1 = "http://localhost:12018//userasync/newtonjsontest";
-                sw.Reset();
-                sw.Start();
-                var res1 = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(hp1.GetStringAsync(test1).Result);
-                sw.Stop();
-                Console.WriteLine("newtonsoft一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
-            }
-
-            System.Threading.Thread.Sleep(100);
-
-            using (HttpClient hp1 = new HttpClient(new HttpClientHandler
-            {
-                AutomaticDecompression =
-    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
-            }))
-            {
-                hp1.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
-                string test1 = "http://localhost:12018//userasync/newtonjsontest";
-                sw.Reset();
-                sw.Start();
-                var res1 = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(hp1.GetStringAsync(test1).Result);
-                sw.Stop();
-                Console.WriteLine("newtonsoft with gzip一共耗时1:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
-            }
-
-            System.Threading.Thread.Sleep(100);
+            Stopwatch sw = new Stopwatch();           
 
             using (HttpClient hp1 = new HttpClient())
             {
@@ -100,6 +42,65 @@ namespace Protobuf_new
                 sw.Stop();
                 Console.WriteLine("jiljson with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
             }
+            System.Threading.Thread.Sleep(100); 
+            using (HttpClient hp = new HttpClient())
+            {
+                hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
+                string test = "http://localhost:12018/protobufnet/2019";
+                sw.Reset();
+                sw.Start();
+                var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
+                sw.Stop();
+                Console.WriteLine("protobuf一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res.Count.ToString());
+            }
+
+            System.Threading.Thread.Sleep(100);
+
+            using (HttpClient hp = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression =
+    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+            }))
+            {
+                hp.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-protobuf; charset=utf-8");
+                string test = "http://localhost:12018/protobufnet/2019";
+                sw.Reset();
+                sw.Start();
+                var res = ProtoBuf.Serializer.Deserialize<List<User>>(hp.GetStreamAsync(test).Result);
+                sw.Stop();
+                Console.WriteLine("protobuf with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res.Count.ToString());
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(100);
+            using (HttpClient hp1 = new HttpClient())
+            {
+                hp1.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                string test1 = "http://localhost:12018//userasync/newtonjsontest";
+                sw.Reset();
+                sw.Start();
+                var res1 = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(hp1.GetStringAsync(test1).Result);
+                sw.Stop();
+                Console.WriteLine("newtonsoft一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
+            }
+
+            System.Threading.Thread.Sleep(100);
+
+            using (HttpClient hp1 = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression =
+    System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+            }))
+            {
+                hp1.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                string test1 = "http://localhost:12018//userasync/newtonjsontest";
+                sw.Reset();
+                sw.Start();
+                var res1 = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(hp1.GetStringAsync(test1).Result);
+                sw.Stop();
+                Console.WriteLine("newtonsoft with gzip一共耗时:" + sw.Elapsed.TotalMilliseconds + "，返回：" + res1.Count.ToString());
+            }
+            Console.WriteLine();
+            
 
             Console.ReadKey();
         }
